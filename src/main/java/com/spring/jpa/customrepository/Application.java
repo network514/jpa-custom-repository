6042af6bd08f45c780da599246b4f9d333eb7f52
network514 <network514@naver.com>
2019-02-19 23:27:10 +0900

    config postfix

--- src/main/java/com/spring/jpa/customrepository/Application.java
+++ src/main/java/com/spring/jpa/customrepository/Application.java
@@ -2,8 +2,11 @@ package com.spring.jpa.customrepository;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+// ~Impl -> ~default로 접미어 변경
+//@EnableJpaRepositories(repositoryImplementationPostfix = "default")
 public class Application {
 
     public static void main(String[] args) {
